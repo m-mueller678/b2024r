8943271dd08b6dc0e51aaa58194e04d406889f69
m-mueller678 <marcus.mueller.678@gmail.com>
2024-04-12 16:56:06 +0200

    implement optimistic memcmp

--- src/access_impl.rs
+++ src/access_impl.rs
@@ -41,7 +41,7 @@ unsafe impl SeqLockMode for Exclusive {
 }
 
 macro_rules! seqlock_primitive {
-    ($(($T:ty) reg=$reg:ident),*) => {
+    ($(($T:ty) reg=$reg:ident reg_f=$reg_f:literal),*) => {
         $(
 
         impl SeqLockGuarded<'_,Optimistic,$T>{
@@ -50,7 +50,7 @@ macro_rules! seqlock_primitive {
                 unsafe{
                     #[cfg(target_arch = "x86_64")]
                     core::arch::asm!(
-                        "mov ({addr}),{dst}",
+                        concat!("mov ({addr:r}),{dst",$reg_f,"}"),
                         addr = in(reg) self.0,
                         dst = lateout($reg) dst
                     );
@@ -72,31 +72,41 @@ macro_rules! seqlock_primitive {
 }
 
 seqlock_primitive!(
-    (u8) reg=reg_byte,
-    (u16) reg=reg,
-    (u32) reg=reg,
-    (u64) reg=reg,
-    (i8) reg=reg_byte,
-    (i16) reg=reg,
-    (i32) reg=reg,
-    (i64) reg=reg
+    (u8) reg=reg_byte reg_f="",
+    (u16) reg=reg reg_f=":x",
+    (u32) reg=reg reg_f=":e",
+    (u64) reg=reg reg_f=":r",
+    (i8) reg=reg_byte reg_f="",
+    (i16) reg=reg reg_f=":x",
+    (i32) reg=reg reg_f=":e",
+    (i64) reg=reg reg_f=":r"
 );
 
 impl<'a> SeqLockGuarded<'a, Optimistic, [u8]> {
     fn cmp(&self, other: &[u8]) -> Ordering {
         let cmp_len = self.to_ptr().len().min(other.len());
-        if cmp_len == 0 || {
+        if cmp_len == 0 {
+            self.to_ptr().len().cmp(&other.len())
+        }else{
+            let result:i8;
             unsafe {
                 core::arch::asm!(
                 "repe cmpsb",
+                "sete {result}",
+                "setcf {neg}",
+                "xor {result}, 1",
+                "sub {result} {neg}",
                 in("si") self.to_ptr() as *mut u8,
                 in("di") other.as_ptr(),
                 in("cx") cmp_len,
+                neg = lateout(reg_byte) _,
+                result = lateout(reg_byte) result,
                 );
+                dbg!(result);
+                debug_assert!((-1 ..= 1).contains(&result));
+                let result = std::mem::transmute::<i8,Ordering>(result);
+                result.then(self.to_ptr().len().cmp(&other.len()))
             }
-            todo!()
-        } {}
-
-        todo!()
+        }
     }
 }
