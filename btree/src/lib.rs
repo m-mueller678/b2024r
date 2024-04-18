4d70176ea2f9b61e91896db02c9a0e27a95497da
m-mueller678 <marcus.mueller.678@gmail.com>
2024-04-18 18:24:18 +0200

    wip

--- btree/src/lib.rs
+++ btree/src/lib.rs
@@ -1,5 +1,6 @@
 use seqlock::{seqlock_wrapper, SeqLockGuarded, SeqLockMode, SeqLockSafe, SeqlockAccessors};
-use std::mem::size_of;
+use std::mem::{offset_of, size_of};
+use std::ptr::slice_from_raw_parts;
 
 pub const PAGE_SIZE: usize = 1 << 12;
 pub const PAGE_HEAD_SIZE: usize = 8;
@@ -33,14 +34,26 @@ pub trait BasicNodeVariant {
     type Upper: SeqLockSafe;
 }
 
+unsafe trait Node{}
+
+impl<N:Node,M:SeqLockMode> Wrapper<SeqLockGuarded<M,N>>{
+    fn slice<T:SeqLockSafe,M:SeqLockMode>(&self,offset:usize,count:usize)->Result<Wrapper<SeqLockGuarded<M,[T]>>,M::ReleaseError>{
+        if offset+count*size_of::<T>() > PAGE_SIZE-PAGE_HEAD_SIZE{
+            return Err(M::release_error())
+        }
+        slice_from_raw_parts(self.as_ptr().cast::<u8>().add(offset).cast::<T>()  ,count)
+    }
+}
+
 impl<V: BasicNodeVariant> BasicNode<V> {
     fn heads<M: SeqLockMode>(
         mut this: Wrapper<SeqLockGuarded<M, Self>>,
     ) -> Result<SeqLockGuarded<M, [u32]>, M::ReleaseError> {
         let count = this.common().count().load();
-        this._data().as_ptr();
+        if offset_of!(Self,data) + size_of::<u32>()*count > PAGE_SIZE{
+            return Err(M::release_error())
+        }
 
-        todo!()
     }
 
     fn find<M: SeqLockMode>(
