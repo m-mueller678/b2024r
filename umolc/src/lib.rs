194b5e93c251d24cfb43e42a04b61ba79be0cf2c
m-mueller678 <marcus.mueller.678@gmail.com>
2024-10-18 16:40:18 +0200

    umolc b-tree wip

--- umolc/src/lib.rs
+++ umolc/src/lib.rs
@@ -1,50 +1,60 @@
+use std::marker::PhantomData;
+use std::mem::{MaybeUninit, transmute};
 use radium::marker::Atomic;
-use radium::Radium;
+use radium::{Atom, Radium};
 use std::ops::{Deref, DerefMut};
 use std::sync::atomic::Ordering::Relaxed;
+use static_assertions::{assert_eq_align, assert_eq_size};
 
-impl<T: Atomic> !Send for OlcAtomic<T> {}
-impl<T: Atomic> !Sync for OlcAtomic<T> {}
-
-struct OlcAtomic<T: Atomic> {
+pub struct OlcAtomic<T: Atomic> {
+    _not_send_sync:PhantomData<*mut u8>,
     x: T::Atom,
 }
 
 impl<T: Atomic> OlcAtomic<T> {
-    fn r(&self) {
+    fn r(&self)->T {
         self.x.load(Relaxed)
     }
 
     fn new(x: T) -> Self {
-        OlcAtomic { x }
+        OlcAtomic { x:T::Atom::new(x),_not_send_sync:PhantomData }
     }
 
-    fn get_mut(&mut self) -> &mut Self {
+    fn get_mut(&mut self) -> &mut T {
         self.x.get_mut()
     }
 }
 
-unsafe trait OlcPage: Sized {}
+pub unsafe trait OlcSafe: Sized {}
 
-struct OlcVersion {
+fn assert_size_align<Src,Dst>(){
+    assert!(size_of::<Src>() == size_of::<Dst>() && align_of::<Src>() >= align_of::<Dst>())
+}
+
+pub fn cast_slice<A:OlcSafe,B:OlcSafe>(a:&[A])->&[B]{
+    assert_size_align::<A,B>();
+
+}
+
+pub struct OlcVersion {
     v: u64,
 }
 
 pub unsafe trait BufferManager<'bm>: 'bm + Copy + Send + Sync + Sized {
-    type Page: OlcPage;
-    type GuardO<'a>: BufferManagerGuard<'bm, Self>
+    type Page: OlcSafe;
+    type GuardO: BufferManagerGuard<'bm, Self>
         + Deref<Target = Self::Page>
         + BufferManageGuardUpgrade<'bm, Self, Self::GuardS>
         + BufferManageGuardUpgrade<'bm, Self, Self::GuardX>;
-    type GuardS<'a>: BufferManagerGuard<'bm, Self>
+    type GuardS: BufferManagerGuard<'bm, Self>
         + Deref<Target = Self::Page>
         + BufferManageGuardUpgrade<'bm, Self, Self::GuardX>;
-    type GuardX<'a>: BufferManagerGuard<'bm, Self> + Deref<Target = Self::Page> + DerefMut;
+    type GuardX: BufferManagerGuard<'bm, Self> + Deref<Target = Self::Page> + DerefMut;
     fn alloc(self) -> Self::GuardX;
     fn free(self, g: Self::GuardX);
 }
 
-trait BufferManagerGuard<'bm, B: BufferManager<'bm>> {
+trait BufferManagerGuard<'bm, B: BufferManager<'bm>>:Sized {
     fn acquire_wait(bm: B, page_id: u64) -> Self;
     fn acquire_wait_version(bm: B, page_id: u64, v: OlcVersion) -> Option<Self> {
         None
@@ -52,6 +62,6 @@ trait BufferManagerGuard<'bm, B: BufferManager<'bm>> {
     fn release(self, bm: B) -> OlcVersion;
 }
 
-trait BufferManageGuardUpgrade<'bm, B: BufferManager<'bm>, Target> {
+trait BufferManageGuardUpgrade<'bm, B: BufferManager<'bm>, Target>:Sized {
     fn try_upgrade(self) -> Result<Target, Self>;
 }
---
+++ umolc_btree/src/node.rs
@@ -0,0 +1,217 @@
+use umolc::OlcAtomic;
+
+pub mod node_tag {
+    pub const METADATA_MARKER: u8 = 43;
+    pub const BASIC_INNER: u8 = 250;
+    pub const BASIC_LEAF: u8 = 251;
+}
+
+#[cfg(feature = "page_1k")]
+pub const PAGE_SIZE: usize = 1024;
+
+#[cfg(feature = "page_4k")]
+pub const PAGE_SIZE: usize = 4096;
+
+pub const NODE_TAIL_SIZE: usize = PAGE_SIZE - size_of::<CommonNodeHead>();
+
+#[repr(C)]
+pub struct CommonNodeHead {
+    pub tag: OlcAtomic<u8>,
+    _pad: OlcAtomic<u8>,
+    pub prefix_len: OlcAtomic<u16>,
+    pub count: OlcAtomic<u16>,
+    pub lower_fence_len: OlcAtomic<u16>,
+    pub upper_fence_len: OlcAtomic<u16>,
+}
+
+#[no_mangle]
+pub unsafe fn print_page(p: *const Page) {
+    todo!();
+}
+
+impl<M: SeqLockMode> Debug for W<Guarded<'_, M, PageTail>> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self.optimistic().node_cast::<BasicLeaf>().tag().load() {
+            node_tag::BASIC_LEAF => Node::format(&self.optimistic().node_cast::<BasicLeaf>(), f),
+            node_tag::BASIC_INNER => Node::format(&self.optimistic().node_cast::<BasicInner>(), f),
+            node_tag::METADATA_MARKER => write!(f, "MetadataPage"),
+            x => write!(f, "UnknownNode{{tag:0x{x:x}}}"),
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct DebugNode<V> {
+    pub prefix_len: usize,
+    pub lf: Vec<u8>,
+    pub uf: Vec<u8>,
+    pub keys: Vec<Vec<u8>>,
+    pub values: Vec<V>,
+}
+
+pub unsafe trait Node: SeqLockWrappable + Pod {
+    const TAG: u8;
+    type DebugVal: Eq + Debug;
+
+    /// fails iff parent_insert fails.
+    /// if node is near empty, no split is performed and parent_insert is not called.
+    fn split<'g, 'bm, BM: BufferManager<'bm>>(
+        this: &mut W<Guarded<'g, Exclusive, Self>>,
+        parent_insert: impl ParentInserter<'bm, BM>,
+        ref_key: &[u8],
+    ) -> Result<(), ()>;
+
+    fn find_separator<'a>(this: &'a W<Guarded<'a, Shared, Self>>, ref_key: &'a [u8]) -> (usize, impl SourceSlice + 'a);
+
+    fn to_debug_kv(this: W<Guarded<Shared, Self>>) -> (Vec<Vec<u8>>, Vec<Self::DebugVal>);
+
+    fn merge(this: &mut W<Guarded<Exclusive, Self>>, right: &mut W<Guarded<Exclusive, Self>>, ref_key: &[u8]);
+    fn format(this: &W<Guarded<Optimistic, Self>>, f: &mut Formatter) -> std::fmt::Result
+    where
+        Self: Copy;
+    fn validate(this: W<Guarded<'_, Shared, Self>>);
+}
+
+#[derive(Clone, Copy, Zeroable, Pod)]
+#[repr(C)]
+pub struct KindInner;
+#[derive(Clone, Copy, Zeroable, Pod)]
+#[repr(C)]
+pub struct KindLeaf;
+
+pub trait ParentInserter<'bm, BM: BufferManager<'bm>> {
+    fn insert_upper_sibling(self, separator: impl SourceSlice) -> Result<Guard<'bm, BM, Exclusive, PageTail>, ()>;
+}
+
+pub trait NodeKind: Pod {
+    const IS_LEAF: bool;
+    type Lower;
+    type SliceType: Pod + SeqLockWrappable;
+    type DebugVal: Eq + Debug;
+
+    fn from_lower(x: Self::Lower) -> [Self::SliceType; 3];
+    fn to_lower(x: [Self::SliceType; 3]) -> Self::Lower;
+    fn to_debug(x: Vec<Self::SliceType>) -> Self::DebugVal;
+}
+
+impl NodeKind for KindInner {
+    const IS_LEAF: bool = false;
+    type Lower = PageId;
+    type SliceType = u16;
+    type DebugVal = PageId;
+
+    fn from_lower(x: Self::Lower) -> [Self::SliceType; 3] {
+        page_id_to_3x16(x)
+    }
+
+    fn to_lower(x: [Self::SliceType; 3]) -> Self::Lower {
+        page_id_from_3x16(x)
+    }
+
+    fn to_debug(x: Vec<Self::SliceType>) -> Self::DebugVal {
+        page_id_from_3x16(x.try_into().unwrap())
+    }
+}
+
+impl NodeKind for KindLeaf {
+    const IS_LEAF: bool = true;
+    type Lower = ();
+    type SliceType = u8;
+    type DebugVal = Vec<u8>;
+
+    fn from_lower(_: Self::Lower) -> [Self::SliceType; 3] {
+        unimplemented!();
+    }
+
+    fn to_lower(_: [Self::SliceType; 3]) -> Self::Lower {
+        unimplemented!();
+    }
+
+    fn to_debug(x: Vec<Self::SliceType>) -> Self::DebugVal {
+        x
+    }
+}
+
+impl<'a, M: SeqLockMode> W<Guarded<'a, M, PageTail>> {
+    pub fn node_cast<N: Node>(self) -> N::Wrapper<Guarded<'a, M, N>> {
+        if M::EXCLUSIVE {
+            debug_assert_eq!(self.s().0.cast::<BasicLeaf>().tag().load(), N::TAG);
+        }
+        self.0.cast::<N>()
+    }
+}
+
+pub fn node_guard_cast<'bm, N: Node, BM: BufferManager<'bm, Page = PageTail>, M: SeqLockMode>(
+    guard: Guard<'bm, BM, M, PageTail>,
+) -> Guard<'bm, BM, M, N> {
+    unsafe { guard.map(|x| x.node_cast::<N>()) }
+}
+
+impl<'a, N: Node> W<Guarded<'a, Shared, N>> {
+    pub fn to_debug(self) -> DebugNode<N::DebugVal> {
+        let (keys, values) = N::to_debug_kv(self);
+        let as_basic = self.cast::<BasicLeaf>();
+        DebugNode {
+            prefix_len: as_basic.prefix_len().load() as usize,
+            lf: as_basic.lower_fence().load_slice_to_vec(),
+            uf: as_basic.upper_fence().load_slice_to_vec(),
+            keys,
+            values,
+        }
+    }
+}
+
+impl<'a, N: Node, M: SeqLockMode> W<Guarded<'a, M, N>> {
+    pub fn upcast(self) -> W<Guarded<'a, M, PageTail>> {
+        self.0.cast::<PageTail>()
+    }
+    pub fn common_head(self) -> W<Guarded<'a, M, CommonNodeHead>> {
+        unsafe { self.0.map_ptr(|x| x as *mut CommonNodeHead) }
+    }
+
+    pub fn slice<T: SeqLockWrappable + Pod>(self, offset: usize, count: usize) -> Guarded<'a, M, [T]> {
+        self.as_bytes().slice(offset..offset + count * size_of::<T>()).cast_slice::<T>()
+    }
+
+    #[allow(clippy::wrong_self_convention)]
+    pub fn as_bytes(self) -> Guarded<'a, M, [u8]> {
+        const SIZE: usize = PAGE_SIZE;
+        self.0.cast::<[u8; SIZE]>().as_slice()
+    }
+
+    pub fn lower_fence(self) -> Guarded<'a, M, [u8]> {
+        let lf = W::rewrap(self.s()).common_head().lower_fence_len().load() as usize;
+        self.slice(size_of::<BasicNodeData>() - lf, lf)
+    }
+
+    pub fn prefix(self) -> Guarded<'a, M, [u8]> {
+        let lf = W::rewrap(self.s()).common_head().lower_fence_len().load() as usize;
+        let pf = W::rewrap(self.s()).common_head().prefix_len().load() as usize;
+        self.slice(size_of::<BasicNodeData>() - lf, pf)
+    }
+
+    pub fn upper_fence(self) -> Guarded<'a, M, [u8]> {
+        let uf = W::rewrap(self.s()).common_head().upper_fence_len().load() as usize;
+        let lf = W::rewrap(self.s()).common_head().lower_fence_len().load() as usize;
+        self.slice(size_of::<BasicNodeData>() - lf - uf, uf)
+    }
+}
+
+pub fn page_id_to_3x16(p: u64) -> [u16; 3] {
+    #[cfg(not(all(target_endian = "little", target_pointer_width = "64")))]
+    compile_error!("only little endian 64-bit is supported");
+    debug_assert!(p < (1 << 48));
+    let a = bytemuck::cast::<[u8; 8], [u16; 4]>(p.to_ne_bytes());
+    [a[0], a[1], a[2]]
+}
+
+pub fn page_id_from_3x16(x: [u16; 3]) -> u64 {
+    let a = bytemuck::cast::<[u16; 4], [u8; 8]>([x[0], x[1], x[2], 0]);
+    u64::from_ne_bytes(a)
+}
+
+#[repr(C, align(8))]
+pub struct Page {
+    pub common: CommonNodeHead,
+    _pad: [OlcAtomic<u8>; NODE_TAIL_SIZE],
+}
